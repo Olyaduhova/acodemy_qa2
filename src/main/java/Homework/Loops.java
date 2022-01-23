20f397fa7ec8027f3ee36be571d4ff5cca9a98d9
Olyaduhova <olyaduhova@gmail.com>
2022-01-23 17:12:27 +0200

    [feature/java3]:Loops. Edit exercise number 2 and 8.

--- src/main/java/Homework/Loops.java
+++ src/main/java/Homework/Loops.java
@@ -7,7 +7,7 @@ public class Loops {
         //1. If variable type is int = 10, print out: "Integer value is 10".
 
         int b = 10;
-        if (b ==10) {
+        if (b == 10) {
             System.out.println("Integer value is " + b);
         }
         System.out.println("\n");
@@ -20,27 +20,20 @@ public class Loops {
         Write code where variable value change allows to get to definite group
         and print message in which you will see in which age group you are and your discount rate size.
          */
-        int myAge = 34;
 
-        int discount10 = 10;
-        int discount15 = 15;
-        int discount20 = 20;
+        int myAge = 66;
 
-        int age1 = 18;
-        int age2 = 21;
-        int age3 = 22;
-        int age4 = 65;
-
-        if (myAge >= age1 && myAge <= age2) {
-            System.out.println("Your age: " + myAge + "\n" + "Your discount: " + discount10);
-        } else if (myAge >= age3 && myAge <= age4) {
-            System.out.println("Your age: " + myAge + "\n" + "Your discount: " + discount15);
+        if (myAge >= 18 && myAge <= 21) {
+            System.out.println("Your age: " + myAge + "\n" + "Your discount: " + "10 %");
+        } else if (myAge >= 22 && myAge <= 65) {
+            System.out.println("Your age: " + myAge + "\n" + "Your discount: " + "15 %");
         } else {
-            if (myAge > age4) {
-                System.out.println("Your age: " + myAge + "\n" + "Your discount: " + discount20);
+            if (myAge > 65) {
+                System.out.println("Your age: " + myAge + "\n" + "Your discount: " + "20%");
             }
         }
         System.out.println("\n");
+
         /*3. Create a String array and add 10 animal names to it.
          a Loop through the array, find and print any animal.
          */
@@ -73,6 +66,7 @@ public class Loops {
         Using switch operator teach the program to print which type of transport you will choose to get to the office,
         according to value in variable.
          */
+
         String weather = "Warm and sunny";
         switch (weather) {
             case "Freezing":
@@ -101,6 +95,7 @@ public class Loops {
                 break;
         }
         System.out.println("\n");
+
         //6. Print numbers from 1 to 100
 
         int number1 = 0;
@@ -119,99 +114,13 @@ public class Loops {
 
         //8. Print multiplication table (from 1 to 10)
 
-        int a = 1;
-        int c = 1;
-        do {
-            System.out.println(a + "*" + c + "=" + a * c);
-            c++;
-        } while (c <= 10);
-        System.out.println("");
-
-        int a2 = 2;
-        int c2 = 2;
-        do {
-            System.out.println(a2 + "*" + c2 + "=" + a2 * c2);
-            c2++;
-        } while (c2 <= 10);
-        System.out.println("");
-
-        int a3 = 3;
-        int c3 = 3;
-        do {
-            System.out.println(a3 + "*" + c3 + "=" + a3 * c3);
-            c3++;
-        } while (c3 <= 10);
-        System.out.println("");
-
-        int a4 = 4;
-        int c4 = 4;
-        do {
-            System.out.println(a4 + "*" + c4 + "=" + a4 * c4);
-            c4++;
-        } while (c4 <= 10);
-        System.out.println("");
-
-        int a5 = 5;
-        int c5 = 5;
-        do {
-            System.out.println(a5 + "*" + c5 + "=" + a5 * c5);
-            c5++;
-        } while (c5 <= 10);
-        System.out.println("");
-
-        int a6 = 6;
-        int c6 = 6;
-        do {
-            System.out.println(a6 + "*" + c6 + "=" + a6 * c6);
-            c6++;
-        } while (c6 <= 10);
-        System.out.println("");
-
-        int a7 = 7;
-        int c7 = 7;
-        do {
-            System.out.println(a7 + "*" + c7 + "=" + a7 * c7);
-            c7++;
-        } while (c7 <= 10);
-        System.out.println("");
-
-        int a8 = 8;
-        int c8 = 8;
-        do {
-            System.out.println(a8 + "*" + c8 + "=" + a8 * c8);
-            c8++;
-        } while (c8 <= 10);
-        System.out.println("");
-
-        int a9 = 9;
-        int c9 = 9;
-        do {
-            System.out.println(a9 + "*" + c9 + "=" + a9 * c9);
-            c9++;
-        } while (c9 <= 10);
-        System.out.println("");
-
-        int a10 = 10;
-        int c10 = 10;
-        do {
-            System.out.println(a10 + "*" + c10 + "=" + a10 * c10);
-            c10++;
-        } while (c10 <= 10);
-        System.out.println("\n");
-
         for (int i = 1; i <= 10; i++) {
-            for (int f = 1; f <= i; f++) {
-                System.out.print(f + "*" + i + "=" + (i * f) + "\t");
+            for (int j = 1; j <= 10; j++) {
+                int result = i * j;
+                System.out.print(result + "\t");
             }
             System.out.println();
 
-//            for (int i = 1; i <= 10; i++) {
-//                for (int j = 1; j <= 10; j++) {
-//                    int result = i * j;
-//                    System.out.print(result + " ");
-//                }
-//                System.out.println();
-//            }
 
         }
 
